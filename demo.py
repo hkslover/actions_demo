cbff07a9cffc771931d5d8e95b61558fd3296bbc
hkslover <38148034+hkslover@users.noreply.github.com>
2020-06-13 17:52:18 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,7 @@
 import requests
 import os
-url = 'https://qmsg.zendee.cn/send/617ff134fba3e73a86cad1cf56ed4c5d'
-response = requests.get('http://wttr.in/baoqing?format=4')
-response = requests.post(url,data = {'msg': response.text})
-print(response.text)
 env_list = os.environ
-print(env_list.get('SNOW'))
+url = 'https://qmsg.zendee.cn/send/617ff134fba3e73a86cad1cf56ed4c5d'
+#response = requests.get('http://wttr.in/baoqing?format=4')
+response = requests.post(url,data = {'msg': env_list.get('SNOW')})
+
