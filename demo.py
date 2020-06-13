e100f7a812ad47f90bd206052b4f4f1428e0564a
hkslover <38148034+hkslover@users.noreply.github.com>
2020-06-13 17:46:07 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
 import requests
+import os
 url = 'https://qmsg.zendee.cn/send/617ff134fba3e73a86cad1cf56ed4c5d'
 response = requests.get('http://wttr.in/baoqing?format=4')
 response = requests.post(url,data = {'msg': response.text})
 print(response.text)
+env_list = os.environ
+print(env_list.get('SNOW'))
