33744537b923e5ab2fef8e87a1544aa7ea076fe5
hkslover <38148034+hkslover@users.noreply.github.com>
2020-06-13 17:55:08 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 import requests
 import os
 env_list = os.environ
-url = 'https://qmsg.zendee.cn/send/617ff134fba3e73a86cad1cf56ed4c5d'
 #response = requests.get('http://wttr.in/baoqing?format=4')
-response = requests.post(url,data = {'msg': env_list.get('SNOW')})
+response = requests.post(env_list.get('QMSG_API'),data = {'msg': '测试消息啊'})
 
