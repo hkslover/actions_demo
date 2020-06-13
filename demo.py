707f9ea068edf481b342dbcdfd75f376b6bd22f1
hkslover <38148034+hkslover@users.noreply.github.com>
2020-06-13 17:31:28 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+import requests
+url = 'https://qmsg.zendee.cn/send/617ff134fba3e73a86cad1cf56ed4c5d'
+response = requests.get('http://wttr.in/baoqing?format=4')
+response = requests.post(url,data = {'msg': response.text})
+print(response.text)
